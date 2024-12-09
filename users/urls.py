2e6ada3ed326af18566fd3ff49b071316d7f49ae
Hussein Naeem <husseinnaeemsec@gmail.com>
2024-12-09 14:14:46 +0300

    Creating authentication views

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+# urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    # path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+]
